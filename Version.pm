1c4f8800307736d4417116b85a0130b70a61c517
Nathaniel J. Graham <broom@cpan.org>
2003-04-05 05:14:33 +0000

    Import of BROOM/File-Version-0.02 from CPAN.
    gitpan-cpan-distribution: File-Version
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         BROOM/File-Version-0.02.tar.gz
    gitpan-cpan-author:       BROOM
    gitpan-cpan-maturity:     released

--- Version.pm
+++ Version.pm
@@ -8,7 +8,7 @@ use integer;
 require Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT = qw(); # nothing to export, this is OO 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 use Carp;
 
 # file-private functions
